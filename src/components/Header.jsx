ec684cfed3cbb4fab2e46d425e1444c1dec5a7a8
Codingvibes8 <wecodeappz@gmail.com>
2025-02-17 06:55:50 +0000

    modify pages and add db models

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,50 +2,49 @@
 
 import { FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+//import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 import { useEffect, useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 export default function Header() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const [searchTerm, setSearchTerm] = useState('');
+  // const searchParams = useSearchParams();
+  // const router = useRouter();
+  // const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(searchParams);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if (searchTermFromUrl) {
-      setSearchTerm(searchTermFromUrl);
-    }
-  }, [searchParams]);
+  // useEffect(() => {
+  //   const urlParams = new URLSearchParams(searchParams);
+  //   const searchTermFromUrl = urlParams.get('searchTerm');
+  //   if (searchTermFromUrl) {
+  //     setSearchTerm(searchTermFromUrl);
+  //   }
+  // }, [searchParams]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams(searchParams);
-    urlParams.set('searchTerm', searchTerm);
-    const searchQuery = urlParams.toString();
-    router.push(`/search?${searchQuery}`);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const urlParams = new URLSearchParams(searchParams);
+  //   urlParams.set('searchTerm', searchTerm);
+  //   const searchQuery = urlParams.toString();
+  //   router.push(`/search?${searchQuery}`);
+  // };
   
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link href='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sahand</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-red-500'>Zenith</span>
+            <span className='text-slate-900'>Estate</span>
           </h1>
         </Link>
         <form
           className='bg-slate-100 p-3 rounded-lg flex items-center'
-          onSubmit={handleSubmit}
+        
         >
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            
           />
           <button>
             <FaSearch className='text-slate-600' />
@@ -62,7 +61,7 @@ export default function Header() {
               About
             </li>
           </Link>
-          <SignedIn>
+          {/* <SignedIn>
             <UserButton />
           </SignedIn>
           <SignedOut>
@@ -71,7 +70,7 @@ export default function Header() {
                 Sign In
               </li>
             </Link>
-          </SignedOut>
+          </SignedOut> */}
         </ul>
       </div>
     </header>
